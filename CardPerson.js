c06e2ea01a9d2a0df960000d17825a098a2b75dd
Jerome Hernandez <hernandez.jrme@gmail.com>
2022-08-19 17:59:33 +0200

    adding miner card design

--- CardPerson.js
+++ CardPerson.js
@@ -24,6 +24,8 @@ const CardPerson = ({image}) => {
         return require('./src/graphic-assets/death.jpg');
       case 'amaterasu':
         return require('./src/graphic-assets/amaterasu.jpg');
+      case 'miner':
+        return require('./src/graphic-assets/miner.jpg');
       default:
         return require('./src/graphic-assets/blank.jpg');
     }
